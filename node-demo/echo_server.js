5202b4245309d9ee122e1c41cf2968e323b5c83b
zhangpei <zhangpei@qutoutiao.net>
2020-07-07 11:44:12 +0800

    promise case

--- node-demo/echo_server.js
+++ node-demo/echo_server.js
@@ -1,26 +1,26 @@
 var net = require('net');
- 
+
 // Configuration parameters
 var HOST = 'localhost';
 var PORT = 12321;
- 
+
 // Create Server instance 
-var server = net.createServer(onClientConnected);  
- 
-server.listen(PORT, HOST, function() {  
+var server = net.createServer(onClientConnected);
+
+server.listen(PORT, HOST, function () {
   console.log('server listening on %j', server.address());
 });
- 
-function onClientConnected(sock) {  
+
+function onClientConnected(sock) {
   var remoteAddress = sock.remoteAddress + ':' + sock.remotePort;
   console.log('new client connected: %s', remoteAddress);
- 
-  sock.on('data', function(data) {
+
+  sock.on('data', function (data) {
     console.log('%s Says: %s', remoteAddress, data);
     sock.write(data);
-    sock.write(' exit');
+    // sock.write(' exit');
   });
-  sock.on('close',  function () {
+  sock.on('close', function () {
     console.log('connection from %s closed', remoteAddress);
   });
   sock.on('error', function (err) {
