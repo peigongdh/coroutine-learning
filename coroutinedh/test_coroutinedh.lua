a4e2ad4cc73330b7a3fe5ed585f4174991ee86a1
zhangpei <peigong.dh@qq.com>
2020-08-22 20:56:11 +0800

    coroutinedh

---
+++ coroutinedh/test_coroutinedh.lua
@@ -0,0 +1,24 @@
+function foo(n)
+    local i = 0
+    while i < 5 do
+        print(coroutine.running())
+        print(n + i)
+        coroutine.yield()
+        i = i + 1
+    end
+end
+
+function test()
+    local co1 = coroutine.create(foo)
+    local co2 = coroutine.create(foo)
+    print("main start")
+    coroutine.resume(co1, 0)
+    coroutine.resume(co2, 100)
+    while coroutine.status(co1) ~= "dead" and coroutine.status(co2) ~= "dead" do
+        coroutine.resume(co1)
+        coroutine.resume(co2)
+    end
+    print("main end")
+end
+
+test()
